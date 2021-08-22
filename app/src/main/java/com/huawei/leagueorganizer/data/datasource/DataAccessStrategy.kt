539dfbe1756a90cb76e2d647ea953cbadf21eba8
okanulkr <okanulkr@hotmail.com>
2021-08-22 22:21:05 +0300

    data access strategy changed

--- app/src/main/java/com/huawei/leagueorganizer/data/datasource/DataAccessStrategy.kt
+++ app/src/main/java/com/huawei/leagueorganizer/data/datasource/DataAccessStrategy.kt
@@ -16,12 +16,16 @@ fun <T, A> performGetOperation(
         val source = databaseQuery.invoke().map { Resource.success(it) }
         emitSource(source)
 
-        val responseStatus = networkCall.invoke()
+        // Below lines simply refreshes data from remote source while user sees old data from database.
+        // I use this logic when I creating social media applications similar to facebook or instagram.
+        // I commented out the lines below because I'm not sure if it's required for assignment.
+
+       /* val responseStatus = networkCall.invoke()
         if (responseStatus.status == Resource.Status.SUCCESS) {
             saveCallResult(responseStatus.data!!)
 
         } else if (responseStatus.status == Resource.Status.ERROR) {
             emit(Resource.error(responseStatus.message!!))
             emitSource(source)
-        }
+        }*/
     }
