74f0bb3e2ef00dba4b4e84f8589d1ab6555b19cb
okanulkr <okanulkr@hotmail.com>
2021-08-22 16:02:37 +0300

    fixture generation algorithm developed

--- app/src/main/java/com/huawei/leagueorganizer/MainActivity.kt
+++ app/src/main/java/com/huawei/leagueorganizer/MainActivity.kt
@@ -4,28 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
-import com.huawei.leagueorganizer.data.entity.TeamEntity
+import com.huawei.leagueorganizer.presentation.viewmodel.MatchViewModel
 import com.huawei.leagueorganizer.presentation.viewmodel.TeamViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val teamViewModel: TeamViewModel by viewModels()
+    private val matchViewModel: MatchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        teamViewModel.teams.observeForever {
-            print("teams")
+        teamViewModel.deleteTeams()
+        teamViewModel.teams.observe(this) {
+            findViewById<TextView>(R.id.tv).text = ""
+            it.data?.forEach { te ->
+                findViewById<TextView>(R.id.tv).append(te.name + "\n")
+            }
+        }
+
+        matchViewModel.matches.observe(this) {
+            print(it)
         }
 
         findViewById<TextView>(R.id.tv).setOnClickListener {
-            teamViewModel.insertTeams(listOf(TeamEntity(0, "GS", "GSLogo")))
+            teamViewModel.deleteTeams()
+            teamViewModel.refresh()
         }
 
         findViewById<TextView>(R.id.tv).setOnLongClickListener {
-            teamViewModel.deleteTeams()
+            teamViewModel.teams.value?.data?.let { it1 -> matchViewModel.generateFixture(it1) }
             true
         }
     }
--- app/src/main/java/com/huawei/leagueorganizer/presentation/viewmodel/MatchViewModel.kt
+++ app/src/main/java/com/huawei/leagueorganizer/presentation/viewmodel/MatchViewModel.kt
@@ -3,6 +3,7 @@ package com.huawei.leagueorganizer.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.huawei.leagueorganizer.data.entity.MatchEntity
+import com.huawei.leagueorganizer.data.entity.TeamEntity
 import com.huawei.leagueorganizer.data.repository.MatchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,6 +13,23 @@ import javax.inject.Inject
 class MatchViewModel @Inject constructor(private val repository: MatchRepository) : ViewModel() {
 
     val matches = repository.matches
+
+    fun generateFixture(teams: List<TeamEntity>) = viewModelScope.launch {
+        repository.deleteMatches()
+
+        val fixture = mutableListOf<MatchEntity>()
+
+        teams.forEach { team1 ->
+            teams.filter { team2 -> team2.id != team1.id }.forEachIndexed { index, team2 ->
+                fixture.add(
+                    if (index % 2 == 0) MatchEntity(0, team1.id, team2.id, index + 1)
+                    else MatchEntity(0, team2.id, team1.id, index + 1)
+                )
+            }
+        }
+
+        repository.insertMatches(fixture)
+    }
 
     fun insertMatches(matches: List<MatchEntity>) = viewModelScope.launch {
         repository.insertMatches(matches)
